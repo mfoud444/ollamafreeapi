6e07e5315933b641273dbda904facafaaf7b18eb
Mohammed Foud <mfoud444@gmail.com>
2025-05-27 01:42:17 +0300

    Initial commit

--- exp.py
+++ exp.py
@@ -6,6 +6,7 @@ print(client.list_models(family='others'))
 # Get info about a specific model
 # print(client.get_model_info('deepseek-r1:7b'))
 
+
 # Chat with a model
 # response = client.chat('deepseek-r1:7b', 'Hello! How are you?', temperature=0.8)
 # print(response) others 
